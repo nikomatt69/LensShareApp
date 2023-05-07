f6911312e7f8a368d027201c4a51010774da7bf0
Nikoemme.eth <nicola.mattioli.95@gmail.com>
2023-05-07 17:43:29 +0200

    feat(create-turbo): apply official-starter transform

--- next.config.js
+++ next.config.js
@@ -20,6 +20,23 @@ const nextConfig = {
   experimental: {
     scrollRestoration: true,
     newNextLinkBehavior: true,
+    turbo: {
+      loaders: {
+        // Option format
+        '.md': [
+          {
+            loader: '@mdx-js/loader',
+            options: {
+              format: 'md',
+            },
+          },
+        ],
+        // Option-less format
+        '.mdx': ['@mdx-js/loader'],
+      },
+    },
+
+
   },
   images: {
     domains: [
